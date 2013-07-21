fcae7960df1b5e28a40a3c4b40e8238c121c4403
Jeff Thompson <mail@jeffreythompson.org>
2013-07-20 22:46:06 -0400

    Lots of general cleanups, clearer printing functions

--- AutomaticSearch/printResult.py
+++ AutomaticSearch/printResult.py
@@ -1,46 +1,38 @@
 
-import json
+import json					# have to reimport since we're getting metadata separately :(
 import requests
 
 # A (somewhat detailed) function to print the result of a query; since
 # it's a bit clunky in our regular code :)
 
 # pass JSON dictionary
-def printResult(searchResults):
+def printDetails(item):
+	print 'Title:       ' + item['title']						# object title
 
-	for item in searchResults['response']['docs']:		# iterate details
-		print 'Title:       ' + item['title']						# object title
-		print 'Format:     ',														# what format is the object in
-		
-		for format in item['format']:
-			print format + ',',
-		print ''
-		
-		print 'Collection: ',														# which collection is the item in?
-		for collection in item['collection']:
-			print collection + ',',
-		print ''
-		
-		# the URL for the listing
-		print 'URL:         www.archive.org/details/' + item['identifier']
+	print 'Format:     ',														# what format is the object in
+	for format in item['format']:
+		print format + ',',
+	print ''
 	
-		# get metadata (also try things like PPI, camera, etc)
-		metadataRequest = requests.post('http://archive.org/metadata/' + item['identifier'])
-		meta = json.loads(metadataRequest.text)
-		print 'Media type:  ' + meta['metadata']['mediatype']	
+	print 'Collection: ',														# which collection is the item in?
+	for collection in item['collection']:
+		print collection + ',',
+	print ''
+	
+	# the URL for the listing
+	print 'URL:         www.archive.org/details/' + item['identifier']
 
-		# get file URL
-		print '\nFile URLs:'
-		index = 0
-		for file in meta['files']:
-			format = file['format']
-			url = 'http://archive.org/download/' + item['identifier'] + '/' + file['name']
-			print str(index) + ':           ' + file['format'] + ' (' + url + ')'
-			index += 1
-		print ''
-	
-		# divider between listings
-		print '- ' * 36
-	
-	# done!
+
+# get metadata (also try things like PPI, camera, etc)
+def printMetadata(meta, identifier):
+	print 'Media type:  ' + meta['metadata']['mediatype']	
+
+	# get file URL
+	print '\nFile URLs:'
+	index = 0
+	for file in meta['files']:
+		format = file['format']
+		url = 'http://archive.org/download/' + identifier + '/' + file['name']
+		print "%02d" % index + ':          ' + file['format'] + ' (' + url + ')'
+		index += 1
 	print ''
