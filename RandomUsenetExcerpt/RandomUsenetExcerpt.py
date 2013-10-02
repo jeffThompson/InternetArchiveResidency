678b230f84919ad3fff5c134e6a2d20f54d15e4f
Jeff Thompson <mail@jeffreythompson.org>
2013-10-02 13:44:53 -0400

    General updates, revised first word search

---
+++ RandomUsenetExcerpt/RandomUsenetExcerpt.py
@@ -0,0 +1,70 @@
+'''
+RANDOM USENET EXCERPT
+Jeff Thompson | 2013 | www.jeffreythompson.org
+
+Via:
+- http://archive.org/details/usenet-com
+
+Requires the Tumblr Python API:
+https://github.com/tumblr/pytumblr
+
+And requires the Python Oauth2 library, found here:
+https://github.com/simplegeo/python-oauth2
+
+TO DO:
++ Post any quotes directly to Twitter (ie start with ", not has a " inside tag)
+
+'''
+
+import pytumblr		# Tumblr API
+import os					# for listing files in directory
+import re					# data parsing
+import shutil			# for moving uploaded folders
+import json				# for parsing settings
+import nltk.data
+import random
+import re
+
+input_filename = 'com.priv.txt'
+num_posts = 100
+blog_address = 'jeff-thompson-iatr.tumblr.com'
+punctuation = re.compile(r'[-.?!,":;()|0-9]')
+
+print '\n\nAUTO POST TO TUMBLR'
+
+# gather some random bits
+# load file and extract sentences
+print 'extracting sentences from text (may take a while)...'
+tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+file = open(input_filename)
+data = file.read()
+excerpts = tokenizer.tokenize(data)
+
+# authenticate via OAuth
+client = pytumblr.TumblrRestClient(
+  'zL2GXi09jX2KcPQcnrJoyXGBUGETAB7bl4TVpYDd4ODB8ZBoMA',
+  'CAjlOmh61JXafXAShg5MtRYHYSqFzWhK4pJ1r98BwLrdXqu2PP',
+  'CvUc32kSnkx8JCVKmKUessqdQBw6Dn3ACGpPuWnURlNKHodC9L',
+  'ZKTolmjfXqxNCynnRkQVYTQQO5gj20F9YXhCRa8v6L10D0epNk'
+)
+
+print 'uploading posts...'
+for i in range(num_posts):
+	print '  ' + str(i+1) + '/' + str(num_posts) + ':',
+	
+	which = random.randrange(0, len(excerpts))
+	
+	text = excerpts[which]
+	tags = []
+	words = re.split('\s+', text.lower())
+	for word in words:
+		tags.append(punctuation.sub("", word))
+	
+	response = client.create_text(blog_address, body=text, tags=tags)
+	if 'id' in response:
+		print '  success!'
+	else:
+		print '  error uploading post, skipping...'
+		# print response
+
+print '\nALL DONE!\n\n'
